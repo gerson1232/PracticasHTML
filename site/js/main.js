4dad2ee44b2c6035f5703497b2b179a37d267a3d
Gerson <gerson.lopez@catolica.edu.sv>
2021-06-29 16:31:49 -0600

    segundo

--- site/js/main.js
+++ site/js/main.js
@@ -32,4 +32,15 @@ var noti = document.querySelector('h1');
 			}
 		})
 	}
+	$('.error-page').hide(0);
+	$('.login-button , .no-access').click(function(){
+		$('.login').slideUp(500);
+		$('.error-page').slideDown(1000);
+	  });
+	  
+	  $('.try-again').click(function(){
+		$('.error-page').hide(0);
+		$('.login').slideDown(1000);
+	  });
+
 	
